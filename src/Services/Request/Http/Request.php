d90530292ab270a82fd60f50e6fc0278f45a659d
Romchik38 <pomahehko.c@gmail.com>
2024-08-30 17:12:07 +0300

    request in progress

---
+++ src/Services/Request/Http/Request.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Romchik38\Server\Request\Http;
+
+use Romchik38\Server\Api\Services\Request\Http\RequestInterface;
+use Romchik38\Server\Api\Services\Request\Http\UriFactoryInterface;
+use Romchik38\Server\Api\Services\Request\Http\UriInterface;
+
+class Request implements RequestInterface
+{
+    public function __construct(
+        protected readonly UriFactoryInterface $uriFactory
+    ) {}
+    public function getMethod(): string
+    {
+        return $_SERVER['REQUEST_METHOD'];
+    }
+
+    public function getUri(): UriInterface
+    {
+        $scheme = $_SERVER['REQUEST_SCHEME'] ?? '';
+        $host = $_SERVER['SERVER_NAME'] ?? '';
+        return $this->uriFactory->create($scheme, $host);
+    }
+}
