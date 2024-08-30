a2ace5aeb1e1ba6bc43c5db8078c149ab090a5f4
Romchik38 <pomahehko.c@gmail.com>
2024-08-30 12:39:02 +0300

    refactor

--- src/Api/Models/Redirect/RedirectRepositoryInterface.php
+++ src/Api/Models/Redirect/RedirectRepositoryInterface.php
@@ -5,8 +5,15 @@ declare(strict_types=1);
 namespace Romchik38\Server\Api\Models;
 
 use Romchik38\Server\Api\Models\RedirectModelInterface;
+use Romchik38\Server\Models\Errors\NoSuchEntityException;
 
 interface RedirectRepositoryInterface extends RepositoryInterface
 {
+    /**
+     * Return a redirectModel entity or throws an error
+     * 
+     * @throws NoSuchEntityException
+     * @return RedirectModelInterface
+     */
     public function checkUrl(string $url, string $method): RedirectModelInterface;
 }
