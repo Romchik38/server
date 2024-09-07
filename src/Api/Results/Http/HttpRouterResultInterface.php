c49c922f117b6bb4c952942a9f0bd6221e1a0e70
Romchik38 <pomahehko.c@gmail.com>
2024-09-07 19:53:14 +0300

    addet consts

--- src/Api/Results/Http/HttpRouterResultInterface.php
+++ src/Api/Results/Http/HttpRouterResultInterface.php
@@ -11,6 +11,12 @@ interface HttpRouterResultInterface extends RouterResultInterface {
     const DEFAULT_HEADERS = [];
     const DEFAULT_STATUS_CODE = 0;
 
+    const METHOD_NOT_ALLOWED_RESPONSE = 'Method Not Allowed';
+    const METHOD_NOT_ALLOWED_CODE = 405;
+
+    const NOT_FOUND_RESPONSE = 'Error 404 from router - Page not found';
+    const NOT_FOUND_STATUS_CODE = 404;
+
     public function getResponse(): string;
     public function getHeaders(): array;
     public function getStatusCode(): int;
