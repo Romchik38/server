d7e4456d6fa9b321e11cace27efddc8fd88e3bfb
Romchik38 <pomahehko.c@gmail.com>
2024-10-28 16:17:33 +0200

    added comments

--- src/Controllers/Errors/DynamicActionNotFoundException.php
+++ src/Controllers/Errors/DynamicActionNotFoundException.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace Romchik38\Server\Controllers\Errors;
 
 /**
- * A route was provided, but dynamic action does not know it
+ * @api
+ * 
+ * Used inside Dynamic Action execute method.
+ * It means - a route was provided, but dynamic action does not know it
+ * The Controller must catch it and deside what to do next
+ * 
  */
-class DynamicActionNotFoundException extends \RuntimeException {
-
-}
+class DynamicActionNotFoundException extends \RuntimeException {}
--- src/Controllers/Errors/NotFoundException.php
+++ src/Controllers/Errors/NotFoundException.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace Romchik38\Server\Controllers\Errors;
 
 /**
- * 404 Error - page not found
+ * @internal
+ * 
+ * Controller throws this exception when the path not found
+ * 
  */
 class NotFoundException extends \RuntimeException {
 
