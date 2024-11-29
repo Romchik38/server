d900b53184d587bb58edc2236d56fbef5f0d9ad7
Romchik38 <pomahehko.c@gmail.com>
2024-11-29 17:34:10 +0200

    fixed class name

--- src/Services/Errors/CantCreateControllerTreeElement.php
+++ src/Services/Errors/CantCreateControllerTreeElement.php
@@ -4,6 +4,4 @@ declare(strict_types=1);
 
 namespace Romchik38\Server\Services\Errors;
 
-class CantCreateControllerTreeElement extends \RuntimeException {
-
-}
+class CantCreateControllerTreeElement extends \RuntimeException {}
