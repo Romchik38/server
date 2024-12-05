e0b4b3f349e2a7892bd45a2239675790622bd37c
Romchik38 <pomahehko.c@gmail.com>
2024-12-05 20:49:20 +0200

    created tests for getDescription

--- tests/unit/Controllers/ControllerTest.php
+++ tests/unit/Controllers/ControllerTest.php
@@ -3,14 +3,12 @@
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
-use Romchik38\Server\Api\Controllers\Actions\ActionInterface;
 use Romchik38\Server\Api\Controllers\Actions\DefaultActionInterface;
 use Romchik38\Server\Api\Controllers\Actions\DynamicActionInterface;
 use Romchik38\Server\Controllers\Actions\Action;
 use Romchik38\Server\Controllers\Controller;
 use Romchik38\Server\Controllers\Errors\DynamicActionLogicException;
 use Romchik38\Server\Models\DTO\DynamicRoute\DynamicRouteDTO;
-use Romchik38\Server\Results\Controller\ControllerResult;
 use Romchik38\Server\Results\Controller\ControllerResultFactory;
 
 class ControllerTest extends TestCase
@@ -36,7 +34,7 @@ class ControllerTest extends TestCase
     public function testGetDescriptionNoActions(): void
     {
         $controller = new Controller('root');
-        $this->assertSame(null, $controller->getDescription());
+        $this->assertSame('root', $controller->getDescription());
     }
 
     public function testGetDescriptionDefaultAction(): void
@@ -61,7 +59,7 @@ class ControllerTest extends TestCase
         $this->assertSame('Home', $controller->getDescription());
     }
 
-    public function testGetDescriptionDinamicAction(): void
+    public function testGetDescriptionDynamicAction(): void
     {
         $action = new class extends Action implements DynamicActionInterface {
             public function execute(string $dynamicRoute): string
@@ -89,6 +87,51 @@ class ControllerTest extends TestCase
             new ControllerResultFactory,
             null,
             $action
+        );
+
+        $this->assertSame('About Page', $controller->getDescription('about'));
+    }
+
+
+    public function testGetDescriptionSetBoth(): void
+    {
+        $defaultAction = new class extends Action implements DefaultActionInterface {
+            public function execute(): string
+            {
+                return 'hello world';
+            }
+            public function getDescription(): string
+            {
+                return 'Home';
+            }
+        };
+
+        $dynamicAction = new class extends Action implements DynamicActionInterface {
+            public function execute(string $dynamicRoute): string
+            {
+                return '<h1>About page</h1>';
+            }
+            public function getDescription(string $dynamicRoute): string
+            {
+                if ($dynamicRoute === 'about') {
+                    return 'About Page';
+                } else {
+                    throw new DynamicActionLogicException('route not exist');
+                }
+            }
+            public function getDynamicRoutes(): array
+            {
+                return [
+                    new DynamicRouteDTO('about', 'Abou Page')
+                ];
+            }
+        };
+        $controller = new Controller(
+            'root',
+            true,
+            new ControllerResultFactory,
+            $defaultAction,
+            $dynamicAction
         );
 
         $this->assertSame('About Page', $controller->getDescription('about'));
